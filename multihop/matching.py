fc7f040512a5719d6dc66ea17824b70a5e3f3cae
mdsrahman <shaifur.at.buet@gmail.com>
2019-04-02 14:34:04 -0400

    first commit, matching functions initiated

---
+++ multihop/matching.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+'''
+Module: matching
+List of entities:
+Use: Contains classes, methods for matching algorithms
+
+'''
+__author__ = "Md Shaifur Rahman"
+__email__ = "mdsrahman@cs.stonybrook.edu"
+__date__ = "April 02, 2019"
+__license__ = "GPL"
+__copyright__ = "Copyright 2019, WINGS Lab, Stony Brook University"
+
+#import numpy as np
+#from scipy import *
+
+#---start here--->
+
+if __name__ == '__main__':
+    '''
+    module test
+    '''
